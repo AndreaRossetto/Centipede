236235e2fdca9bdb04b6281f08b76f2fc004d1a3
marcellocolameo <marcellocolameo@gmail.com>
2016-06-25 17:43:11 +0200

    Update Breakout_KEYBOARD.vhd
    commenti

--- Breakout_KEYBOARD.vhd
+++ Breakout_KEYBOARD.vhd
@@ -3,6 +3,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
+-- clk & keyboardClock sono rispettivamente il clock generale e quello della tastiera
+-- keyboardData: dato che ricevo
+-- keyCode: quando voglio inviare un carattere 
+
+
+--Funzionamento tastiera:
+--quando un tasto è premuto, il make code viene inviato al 'pc'
+--quando un tasto è rilasciato il breakcode viene mandato
+
+
 
 entity Breakout_KEYBOARD is
     Port ( 	clk : in STD_LOGIC;
@@ -13,12 +23,17 @@ end Breakout_KEYBOARD;
 
 architecture Behavioral of Breakout_KEYBOARD is
 
+
+
 signal bitCount : integer range 0 to 100 := 0;
 signal scancodeReady : STD_LOGIC := '0';
 signal scancode : STD_LOGIC_VECTOR(7 downto 0);
 signal breakReceived : STD_LOGIC_VECTOR(1 downto 0) := "00";
 
 begin
+
+--Tastiera in seriale, da 1 a 8 sono i dati (8 bit), poi c'e' quelli di parità e poi quello fine messaggio
+
 
 	Keyboard : process(keyboardClock)
 	begin
@@ -29,16 +44,19 @@ begin
 			elsif bitCount > 0 and bitCount < 9 then -- shift di un bit nello scancode da sinistra
 				scancode <= keyboardData & scancode(7 downto 1);
 				bitCount <= bitCount + 1;
-			elsif bitCount = 9 then -- bit di parit�
+			elsif bitCount = 9 then -- bit di parità
 				bitCount <= bitCount + 1;
 			elsif bitCount = 10 then -- fine messaggio
-				scancodeReady <= '1';
-				bitCount <= 0;
+				scancodeReady <= '1';   --pronto per mandare
+				bitCount <= 0;  --riazzero per ulteriore carattere
 			end if;
 		end if;		
 	end process Keyboard;
 	
-	
+--una volta ricevuti tutti i bit, mi salvo il tipo di tasto premuto( 01/10) 
+
+
+
 	DataSend : process(scancodeReady, scancode)
 	begin
 		if scancodeReady'event and scancodeReady = '1' then
@@ -62,3 +80,5 @@ begin
 	
 	
 end Behavioral;
+
+
