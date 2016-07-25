7d0de14e5c3b99d65bb6c905abc86a7a196909c1
AndreaRossetto <andrea.rossetto2@studio.unibo.it>
2016-07-25 16:55:06 +0200

    Update Centipede_DATA.vhd

--- new/Centipede_DATA.vhd
+++ new/Centipede_DATA.vhd
@@ -36,26 +36,28 @@ ARCHITECTURE behavior of  Centipede_DATA IS
 BEGIN
 	
 PROCESS
-variable bulletPositionV: integer range 0 to 1000:=250;
-variable bulletPositionH: integer range 0 to 1000:=250;
+--posizioni proiettile
+variable bulletPositionV: integer range 0 to 480:=250;
+variable bulletPositionH: integer range 0 to 600:=250;
+--variabile di gameOver
 variable gameO: STD_LOGIC :='0';
 variable cntScrittaLampeggiante: integer range 0 to 16000000;
 variable scrittaLampeggia: STD_LOGIC;
 variable padHorizontalDimension:  integer range 0 to 200:=10;
-variable padLeftCorner: integer  range 0 to 1000:= 300;
-variable padRightCorner: integer  range 0 to 1000:=308;
+variable padLeftCorner: integer  range 0 to 600:= 300;
+variable padRightCorner: integer  range 0 to 600:=308;
 
 -- bordo schermo
-constant leftBorder:integer  range 0 to 1000:=45;
-constant rightBorder:integer  range 0 to 1000:=606; -- 606
-constant upBorder: integer  range 0 to 500:=30;	
+constant leftBorder:integer  range 0 to 50:=45;
+constant rightBorder:integer  range 0 to 640:=606; -- 606
+constant upBorder: integer  range 0 to 50:=30;	
 constant downBorder: integer  range 0 to 500:= 460;
 
 variable cont: INTEGER range 0 to 10 := 0; 
 variable youWin: STD_LOGIC:='0';
 variable toShooted: std_logic :='0';
 variable toSnake: std_logic :='0';
-variable indM: INTEGER range 0 to 30 := 2;
+--indicano le direzioni(1 destra, 0 sinistra) degli elementi del serpente
 variable dirR0: STD_LOGIC := '1';
 variable dirR1: STD_LOGIC := '1';
 variable dirR2: STD_LOGIC := '1';
@@ -63,9 +65,12 @@ variable dirR3: STD_LOGIC := '1';
 variable dirR4: STD_LOGIC := '1';
 variable dirR5: STD_LOGIC := '1';
 variable dirR6: STD_LOGIC := '1';
-variable dirR7: STD_LOGIC := '1';
+
+--indica se il proiettile ha colpito un fungo
 variable mushroomS: STD_LOGIC;
+--indica se il proiettile ha colpito il serpente
 variable snakeS: STD_LOGIC;
+--elementi del serpente
 variable centi0: coord_type;
 variable centi1: coord_type;
 variable centi2: coord_type;
@@ -74,25 +79,26 @@ variable centi4: coord_type;
 variable centi5: coord_type;
 variable centi6: coord_type;
 
+--se il colpo sta viaggiando(sparato)
 variable shooted: STD_LOGIC := '0';
 variable vScore2: INTEGER range -9 to 9 := 0;
 variable vScore1: INTEGER range -9 to 9 := 0; 
 variable vScore0: INTEGER range -9 to 9 := 0; 
 
+--variabile di supporto utili per calcolare le coordinate reali della cella del vettore
 variable row: integer range 0 to 500;
 variable col: integer range 0 to 5000;
 variable comp: integer range 0 to 5000;
 
-variable mushroom: STD_LOGIC_VECTOR(2596 downto 0); 
-
-variable indF: INTEGER range 0 to 2596;
+--vettore con i funghi
+variable mushroom: STD_LOGIC_VECTOR(2596 downto 0);
 
 BEGIN
 WAIT UNTIL(clk'EVENT) AND (clk = '1');
 
 		IF(cntScrittaLampeggiante = 10000000)THEN	
 				cntScrittaLampeggiante := 0;
-				scrittaLampeggia :=NOT scrittaLampeggia;		
+				scrittaLampeggia := NOT scrittaLampeggia;		
 			ELSE	
 				cntScrittaLampeggiante := cntScrittaLampeggiante  + 1;
 		END IF;
@@ -110,30 +116,27 @@ WAIT UNTIL(clk'EVENT) AND (clk = '1');
 				mushroom(i) := '0';
 			end loop;
 			
-			mushroom(0) := '1';
-			mushroom(10) := '1';
-			mushroom(200) := '1';
-			mushroom(130) := '1';
 			
-			mushroom(400) := '1';
-			mushroom(150) := '1';
-			mushroom(180) := '1';
-			mushroom(250) := '1';
-			mushroom(323) := '1';
-			mushroom(500) := '1';
-			mushroom(510) := '1';
-			mushroom(860) := '1';
-			mushroom(915) := '1';
-			mushroom(415) := '1';
-			mushroom(310) := '1';
-			mushroom(1500) := '1';
-			mushroom(1050) := '1';
-			mushroom(1450) := '1';
-			mushroom(1200) := '1';
-			mushroom(1225) := '1';
-			mushroom(300) := '1';
-			mushroom(700) := '1';
+			--assegnamento delle posizioni dei funghi
+			mushroom(0) := '1';
+			mushroom(178) := '1';
+			mushroom(436) := '1';			
 			mushroom(520) := '1';
+			mushroom(593) := '1';
+			mushroom(369) := '1';
+			mushroom(610) := '1';
+			mushroom(652) := '1';
+			mushroom(840) := '1';
+			mushroom(795) := '1';
+			mushroom(964) := '1';
+			mushroom(954) := '1';			
+			mushroom(1160) := '1';
+			mushroom(1050) := '1';
+			mushroom(1225) := '1';
+			mushroom(1450) := '1';
+			mushroom(1480) := '1';
+			mushroom(1500) := '1';			
+			mushroom(2345) := '1';
 			
 			dirR0 := '1';
 			dirR1 := '1';
@@ -142,10 +145,10 @@ WAIT UNTIL(clk'EVENT) AND (clk = '1');
 			dirR4 := '1';
 			dirR5 := '1';
 			dirR6 := '1';
-			dirR7 := '1';
 			
 			shooted := '0';
-						
+			
+			--assegnamento posizione iniziale del serpente	
 			centi0.x := 40;
 			centi0.y := 0;
 			centi1.x := -1;
@@ -165,17 +168,18 @@ WAIT UNTIL(clk'EVENT) AND (clk = '1');
 			padLeftCorner:=300;
 			padRightCorner:=308;
 			bulletPositionH := padLeftCorner+5;
-			gameO :='0'; 
+			gameO :='0';
 			youWin:='0';
 			
 			northBorder <= upBorder;
 			southBorder <= downBorder;
-			westBorder <= leftBorder;
-			eastBorder <= rightBorder;
+			westBorder  <= leftBorder;
+			eastBorder  <= rightBorder;
 		END IF;
 		
 		goingReady<='0';
 		
+		--riposizionamento del proiettile
 		if shooted = '0' then
 			bulletPositionH := padLeftCorner+5;
 		end if;
@@ -189,6 +193,7 @@ WAIT UNTIL(clk'EVENT) AND (clk = '1');
 			toSnake:='1'; 
 		end if;
 		
+		--tutta la gestione del movimento del serpente
 		IF(toSnake='1') THEN			
 			case cont is
 					when 0 => 
@@ -197,13 +202,13 @@ WAIT UNTIL(clk'EVENT) AND (clk = '1');
 						centi1.x := centi0.x;
 						centi1.y := centi0.y - 10;
 						cont := cont + 1;
-					when 2 => 
+					when 2 =>
 						centi2.x := centi1.x;
 						centi2.y := centi1.y - 10;
 						centi1.x := centi0.x;
 						centi1.y := centi0.y - 10;
 						cont := cont + 1;
-					when 3 => 
+					when 3 =>
 						centi3.x := centi2.x;
 						centi3.y := centi2.y - 10;
 						centi2.x := centi1.x;
@@ -247,7 +252,7 @@ WAIT UNTIL(clk'EVENT) AND (clk = '1');
 						centi1.x := centi0.x;
 						centi1.y := centi0.y - 10;
 						cont := cont + 1;
-					when others =>  -- MANCA CONTROLLO SERPENTE
+					when others => 
 						if centi0.x /= -1 then
 							if dirR0 = '1' then
 								if centi0.y >= rightBorder-18 then
@@ -367,7 +372,8 @@ WAIT UNTIL(clk'EVENT) AND (clk = '1');
 								end if;
 							end if;
 						end if;
-						for i in 0 to 2596 loop
+
+						for i in 0 to 2205 loop
 							if mushroom(i) = '1' then
 								row := 45 + 10 * (i / 59);
 								comp := 59 * (i / 59);
@@ -390,113 +396,113 @@ WAIT UNTIL(clk'EVENT) AND (clk = '1');
 									end if;
 								end if;
 								
---								if centi1.x /= -1 then
---									if dirR1 = '1' then
---										if centi1.y+8 >= col+2 then
---											if centi1.x+8 <= row+3 AND centi1.x-2 >= row-15 then
---												centi1.x := centi1.x + 10;
---												dirR1 := '0';
---											end if;
---										end if;
---									else
---										if centi1.y <= col+9 then
---											if centi1.x+8 <= row+3 AND centi1.x-2 >= row-15 then
---												centi1.x := centi1.x + 10;
---												dirR1 := '1';
---											end if;
---										end if;
---									end if;
---								end if;
---
---								if centi2.x /= -1 then
---									if dirR2 = '1' then
---										if centi2.y+8 >= col+2 then
---											if centi2.x+8 <= row+3 AND centi2.x-2 >= row-15 then
---												centi2.x := centi2.x + 10;
---												dirR2 := '0';
---											end if;
---										end if;
---									else
---										if centi2.y <= col+9 then
---											if centi2.x+8 <= row+3 AND centi2.x-2 >= row-15 then
---												centi2.x := centi2.x + 10;
---												dirR2 := '1';
---											end if;
---										end if;
---									end if;
---								end if;
---								
---								if centi3.x /= -1 then
---									if dirR3 = '1' then
---										if centi3.y+8 >= col+2 then
---											if centi3.x+8 <= row+3 AND centi3.x-2 >= row-15 then
---												centi3.x := centi3.x + 10;
---												dirR3 := '0';
---											end if;
---										end if;
---									else
---										if centi3.y <= col+9 then
---											if centi3.x+8 <= row+3 AND centi3.x-2 >= row-15 then
---												centi3.x := centi3.x + 10;
---												dirR3 := '1';
---											end if;
---										end if;
---									end if;
---								end if;
---
---								if centi4.x /= -1 then
---									if dirR4 = '1' then
---										if centi4.y+8 >= col+2 then
---											if centi4.x+8 <= row+3 AND centi4.x-2 >= row-15 then
---												centi4.x := centi4.x + 10;
---												dirR4 := '0';
---											end if;
---										end if;
---									else
---										if centi4.y <= col+9 then
---											if centi4.x+8 <= row+3 AND centi4.x-2 >= row-15 then
---												centi4.x := centi4.x + 10;
---												dirR4 := '1';
---											end if;
---										end if;
---									end if;
---								end if;
---
---								if centi5.x /= -1 then
---									if dirR5 = '1' then
---										if centi5.y+8 >= col+2 then
---											if centi5.x+8 <= row+3 AND centi5.x-2 >= row-15 then
---												centi5.x := centi5.x + 10;
---												dirR5 := '0';
---											end if;
---										end if;
---									else
---										if centi5.y <= col+9 then
---											if centi5.x+8 <= row+3 AND centi5.x-2 >= row-15 then
---												centi5.x := centi5.x + 10;
---												dirR5 := '1';
---											end if;
---										end if;
---									end if;
---								end if;
---								
---								if centi6.x /= -1 then
---									if dirR6 = '1' then
---										if centi6.y+8 >= col+2 then
---											if centi6.x+8 <= row+3 AND centi6.x-2 >= row-15 then
---												centi6.x := centi6.x + 10;
---												dirR6 := '0';
---											end if;
---										end if;
---									else
---										if centi6.y <= col+9 then
---											if centi6.x+8 <= row+3 AND centi6.x-2 >= row-15 then
---												centi6.x := centi6.x + 10;
---												dirR6 := '1';
---											end if;
---										end if;
---									end if;
---								end if;
+								if centi1.x /= -1 then
+									if dirR1 = '1' then
+										if centi1.y+8 >= col+2 then
+											if centi1.x+8 <= row+3 AND centi1.x-2 >= row-15 then
+												centi1.x := centi1.x + 10;
+												dirR1 := '0';
+											end if;
+										end if;
+									else
+										if centi1.y <= col+9 then
+											if centi1.x+8 <= row+3 AND centi1.x-2 >= row-15 then
+												centi1.x := centi1.x + 10;
+												dirR1 := '1';
+											end if;
+										end if;
+									end if;
+								end if;
+
+								if centi2.x /= -1 then
+									if dirR2 = '1' then
+										if centi2.y+8 >= col+2 then
+											if centi2.x+8 <= row+3 AND centi2.x-2 >= row-15 then
+												centi2.x := centi2.x + 10;
+												dirR2 := '0';
+											end if;
+										end if;
+									else
+										if centi2.y <= col+9 then
+											if centi2.x+8 <= row+3 AND centi2.x-2 >= row-15 then
+												centi2.x := centi2.x + 10;
+												dirR2 := '1';
+											end if;
+										end if;
+									end if;
+								end if;
+								
+								if centi3.x /= -1 then
+									if dirR3 = '1' then
+										if centi3.y+8 >= col+2 then
+											if centi3.x+8 <= row+3 AND centi3.x-2 >= row-15 then
+												centi3.x := centi3.x + 10;
+												dirR3 := '0';
+											end if;
+										end if;
+									else
+										if centi3.y <= col+9 then
+											if centi3.x+8 <= row+3 AND centi3.x-2 >= row-15 then
+												centi3.x := centi3.x + 10;
+												dirR3 := '1';
+											end if;
+										end if;
+									end if;
+								end if;
+
+								if centi4.x /= -1 then
+									if dirR4 = '1' then
+										if centi4.y+8 >= col+2 then
+											if centi4.x+8 <= row+3 AND centi4.x-2 >= row-15 then
+												centi4.x := centi4.x + 10;
+												dirR4 := '0';
+											end if;
+										end if;
+									else
+										if centi4.y <= col+9 then
+											if centi4.x+8 <= row+3 AND centi4.x-2 >= row-15 then
+												centi4.x := centi4.x + 10;
+												dirR4 := '1';
+											end if;
+										end if;
+									end if;
+								end if;
+
+								if centi5.x /= -1 then
+									if dirR5 = '1' then
+										if centi5.y+8 >= col+2 then
+											if centi5.x+8 <= row+3 AND centi5.x-2 >= row-15 then
+												centi5.x := centi5.x + 10;
+												dirR5 := '0';
+											end if;
+										end if;
+									else
+										if centi5.y <= col+9 then
+											if centi5.x+8 <= row+3 AND centi5.x-2 >= row-15 then
+												centi5.x := centi5.x + 10;
+												dirR5 := '1';
+											end if;
+										end if;
+									end if;
+								end if;
+								
+								if centi6.x /= -1 then
+									if dirR6 = '1' then
+										if centi6.y+8 >= col+2 then
+											if centi6.x+8 <= row+3 AND centi6.x-2 >= row-15 then
+												centi6.x := centi6.x + 10;
+												dirR6 := '0';
+											end if;
+										end if;
+									else
+										if centi6.y <= col+9 then
+											if centi6.x+8 <= row+3 AND centi6.x-2 >= row-15 then
+												centi6.x := centi6.x + 10;
+												dirR6 := '1';
+											end if;
+										end if;
+									end if;
+								end if;
 								
 							end if;
 						end loop;
@@ -516,10 +522,9 @@ WAIT UNTIL(clk'EVENT) AND (clk = '1');
 			bulletPositionV := bulletPositionV - 1;
 			mushroomS := '0';
 			snakeS := '0';
-			indF := 0;
 			
 			-- bullet crosses mushrooms
-			for i in 0 to 2596 loop
+			for i in 0 to 2205 loop
 				row := 45 + 10 * (i / 59);
 				comp := 59 * (i / 59);
 				col := 60 + 10 * (i-comp);
@@ -553,55 +558,55 @@ WAIT UNTIL(clk'EVENT) AND (clk = '1');
 			if( bulletPositionH >= centi6.y AND bulletPositionH <= centi6.y+10 AND bulletPositionV <= centi6.x+8 ) then
 				snakeS := '1';
 			end if;
-			for i in 0 to 2596 loop
-				row := 45 + 10 * (i / 59);
-				comp := 59 * (i / 59);
-				col := 60 + 10 * (i-comp);
-				if mushroom(i) = '0' then
-					if row >= centi0.x+10 AND row <= centi0.x+20 then
-						if col >= centi0.y+10  then
-							indF := i;
-						end if;
-					end if;
-				end if;
-			end loop;
+
 			-- ogni pezzo del serpente fornisce 100 punti
 			if snakeS = '1' then
 				vScore2 := vScore2 + 1;
 				if centi6.x /= -1 then
-					
+					mushroom(1829) := '1';
+					mushroom(1832) := '1';
+					mushroom(1833) := '1';
 					centi6.x := -1;
 					centi6.y := -1;
 				elsif centi5.x /= -1 then
-					
+					mushroom(1973) := '1';
+					mushroom(1977) := '1';
+					mushroom(1978) := '1';
 					centi5.x := -1;
 					centi5.y := -1;
 				elsif centi4.x /= -1 then
-					
+					mushroom(2105) := '1';
+					mushroom(2106) := '1';
+					mushroom(2108) := '1';
 					centi4.x := -1;
 					centi4.y := -1;
 				elsif centi3.x /= -1 then
-					
+					mushroom(2158) := '1';
+					mushroom(2160) := '1';
+					mushroom(2161) := '1';
 					centi3.x := -1;
 					centi3.y := -1;
 				elsif centi2.x /= -1 then
-					
+					mushroom(2190) := '1';
+					mushroom(2192) := '1';
+					mushroom(2194) := '1';
 					centi2.x := -1;
 					centi2.y := -1;
 				elsif centi1.x /= -1 then
-					
+					mushroom(2201) := '1';
+					mushroom(2202) := '1';
+					mushroom(2204) := '1';
 					centi1.x := -1;
 					centi1.y := -1;
 				elsif centi0.x /= -1 then
-					
 					centi0.x := -1;
 					centi0.y := -1;
 				end if;
-				mushroom(indF) := '1';
+				
 			end if;
 
 			if mushroomS = '1' then
-				-- ogni fungo tolgie 20 punti
+				-- ogni fungo toglie 20 punti
 				if vScore1 = 0 AND vScore2 > 0 then
 					vScore1 := 8;
 					vScore2 := vScore2 - 1;
@@ -631,7 +636,7 @@ WAIT UNTIL(clk'EVENT) AND (clk = '1');
 		toShooted := '0';
 		
 		IF(padGoRight = '0' AND padGoLeft = '1') THEN
-			if (padRightCorner = rightBorder-15) THEN
+			if (padRightCorner = rightBorder-106) THEN
 					padLeftCorner:=padLeftCorner;
 				else padLeftCorner := padLeftCorner+1;
 			end if;	
